bcece7ae7b0754e085d6d2bb5ddfbab4cd97bc7d
kath3rine <not.katherine.li@gmail.com>
2023-01-19 17:54:39 -0500

    big update

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
- function MM_swapImgRestore() { //v3.0
-  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+function swapImg(image, source) {
+    document.getElementById(image).src = source;
 }
 
-function MM_findObj(n, d) { //v4.01
-  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
-    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
-  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
-  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
-  if(!x && d.getElementById) x=d.getElementById(n); return x;
+function swapText(text, source) {
+    document.getElementById(text).innerHTML = source;
 }
 
-function MM_swapImage() { //v3.0
-  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
-   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+function updateURL(url, source) {
+    document.getElementById(url).href = source;
+   }
+
+function captionOn(text, image) {
+    document.getElementById(text).style.color = 'var(--col4)';
+    document.getElementById(image).style.opacity = 0.15;
 }
 
-function MM_showHideLayers() { //v9.0
-    var i,p,v,obj,args=MM_showHideLayers.arguments;
-    for (i=0; i<(args.length-2); i+=3) 
-    with (document) if (getElementById && ((obj=getElementById(args[i]))!=null)) { v=args[i+2];
-      if (obj.style) { obj=obj.style; v=(v=='show')?'visible':(v=='hide')?'hidden':v; }
-      obj.visibility=v; }
-  }
-  function MM_setTextOfLayer(objId,x,newText) { //v9.0
-    with (document) if (getElementById && ((obj=getElementById(objId))!=null))
-      with (obj) innerHTML = unescape(newText);
-  }
+function captionOff(text, image) {
+    document.getElementById(text).style.color = 'transparent';
+    document.getElementById(image).style.opacity = 1;
+}
 
-  function updateURL1() {
-    document.getElementById("caption").href = "https://www.youtube.com/watch?v=Jx_pdBmf2CU";
-  }
+function linkOn(link, color) {
+    document.getElementById(link).style.color = color;
+}
 
- function updateURL2() {
-  document.getElementById("caption").href = "https://drive.google.com/file/d/1uK8ABwFzT2C6JsXKBH8Z84MAbpqcaxB3/view?usp=share_link";
- }
+function linkOff(link) {
+    document.getElementById(link).style.color = 'transparent'
+}
